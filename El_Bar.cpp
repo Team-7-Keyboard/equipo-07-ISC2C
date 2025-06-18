c124342c958e87bc6c60f24ffcdd291c19cc0173
DiegoPalomino <pepe313692xp@gmail.com>
2025-06-17 19:10:28 -0600

    Primera version de el Ejercicio del Bar

---
+++ El_Bar.cpp
@@ -0,0 +1,22 @@
+//Ejercicio #4 – El bar
+#include <iostream>
+
+using namespace std;
+
+struct Datos{
+	char categoria;
+	float valor;
+	float ventaMax,ventaMin;
+};
+
+Datos bar;
+void menu();
+
+int main(){
+	menu();
+}
+
+void menu(){
+	cout <<"\tBAR -EL PEPE-"<<endl;
+	
+}
