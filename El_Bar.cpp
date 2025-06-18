b4df3c92031ec5cdbdb6fa434225d3425a7d5e0c
DiegoPalomino <pepe313692xp@gmail.com>
2025-06-17 21:32:11 -0600

    Nuevas mejoras en El_Bar.cpp

--- El_Bar.cpp
+++ El_Bar.cpp
@@ -6,10 +6,12 @@ using namespace std;
 struct Datos{
 	char categoria;
 	float valor;
-	float ventaMax,ventaMin;
+	float ventaMax=0,ventaMin=1000;
+	char catAux;
+	float valAux;
 };
 
-Datos bar;
+
 int menu();
 
 int main(){
@@ -17,6 +19,7 @@ int main(){
 }
 
 int menu(){
+	Datos barDatos;
 	cout <<"\tBAR -EL PEPE-"<<endl;
 	cout << "Se debe de tener la lista de las ventas en un archivo de texto llamado bar.txt"<<endl;
 	cout<< "Se indica que para indicar que termino el dia, en categoria indicar N y en valor un 0"<<endl;
@@ -24,7 +27,7 @@ int menu(){
 	FILE *bar = fopen("bar.txt","r");
 	if(bar==NULL){
 		cout <<"Error al abrir el archivo";
-		return -1;
+		return 1;
 	}else{
 		
 	cout <<"\tMenu:\n"<<endl;
@@ -33,6 +36,23 @@ int menu(){
 	cout <<"Comidas - A"<<endl;
 	cout <<"Meriendas - M"<<endl;
 	cout <<"Copas - C"<<endl;
-}
 
+	
+	while (!feof(bar)){
+
+	fscanf(bar, "%c\t%d", &barDatos.categoria, &barDatos.valor);
+		if (barDatos.categoria != 'N' && barDatos.categoria != 'n' ){
+		barDatos.valAux= barDatos.valor;
+		if(barDatos.valor < barDatos.ventaMin){
+			barDatos.ventaMin = barDatos.valor;
+			barDatos.catAux = barDatos.categoria;
+			}
+		if(barDatos.valor > barDatos.ventaMax){
+			barDatos.ventaMax = barDatos.valor;
+			barDatos.catAux = barDatos.categoria;
+			}	
+		}
+    }
+	 fclose(bar);
+	}
 }
