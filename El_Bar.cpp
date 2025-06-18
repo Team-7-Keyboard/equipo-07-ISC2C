837eb21ce461bfd8d57e8778453b6b19af97ae9a
DiegoPalomino <pepe313692xp@gmail.com>
2025-06-17 20:56:54 -0600

    Actualizar El_Bar.cpp con nuevos cambios

--- El_Bar.cpp
+++ El_Bar.cpp
@@ -10,13 +10,29 @@ struct Datos{
 };
 
 Datos bar;
-void menu();
+int menu();
 
 int main(){
 	menu();
 }
 
-void menu(){
+int menu(){
 	cout <<"\tBAR -EL PEPE-"<<endl;
+	cout << "Se debe de tener la lista de las ventas en un archivo de texto llamado bar.txt"<<endl;
+	cout<< "Se indica que para indicar que termino el dia, en categoria indicar N y en valor un 0"<<endl;
+
+	FILE *bar = fopen("bar.txt","r");
+	if(bar==NULL){
+		cout <<"Error al abrir el archivo";
+		return -1;
+	}else{
+		
+	cout <<"\tMenu:\n"<<endl;
 	
+	cout <<"Desayuno - D"<<endl;
+	cout <<"Comidas - A"<<endl;
+	cout <<"Meriendas - M"<<endl;
+	cout <<"Copas - C"<<endl;
+}
+
 }
